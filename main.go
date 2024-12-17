a4150dc6419c5b61b2a93cddbdab84e6ab70fde3
Nilesh-567 <137327289+Nilesh-567@users.noreply.github.com>
2024-12-17 12:58:30 +0530

    Update main.go

--- main.go
+++ main.go
@@ -69,7 +69,7 @@ func signUpHandler(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(map[string]string{"message": "User registered successfully"})
 }
 
-fun signupHandlers() {
+func signupHandlers() {
 	fmt.Printf("get method responds successfully ")
 }
 
